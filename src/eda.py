5e9eacf2784054a93f43e40a81b7ab4114f34d95
Kelly Lutz <Kelly@MacBook-Air.local>
2018-06-26 14:29:02 -0600

    Update labels on plots

--- src/eda.py
+++ src/eda.py
@@ -19,12 +19,12 @@ for df in [routes,boulders]:
     df.drop('Unnamed: 0',axis=1,inplace=True)
 
 def plot_routes():
-    rpg = sns.countplot(x='usa_routes', hue ='sex', data=routes)
-    handles, _ = rpg.get_legend_handles_labels()
-    rpg.legend(handles, ["Male", "Female"])
-    rpg.set_xticklabels(['5.4','5.5','5.6','5.7','5.8','5.9','5.10a','5.10b','5.10c','5.10d','5.11a','5.11b','5.11c','5.12a','5.12b','5.12c','5.12d','5.13a','5.13b','5.13c','5.13d','5.14a','5.14b','5.14c','5.14d','5.15a'])
-    rpg.set_title('Routes per Gender')
-    for tick in rpg.get_xticklabels():
+    route_per_gender = sns.countplot(x='usa_routes', hue ='sex', data=routes)
+    handles, _ = route_per_gender.get_legend_handles_labels()
+    route_per_gender.legend(handles, ["Male", "Female"])
+    route_per_gender.set_xticklabels(['5.4','5.5','5.6','5.7','5.8','5.9','5.10a','5.10b','5.10c','5.10d','5.11a','5.11b','5.11c','5.12a','5.12b','5.12c','5.12d','5.13a','5.13b','5.13c','5.13d','5.14a','5.14b','5.14c','5.14d','5.15a'])
+    route_per_gender.set_title('Route Grade per Gender')
+    for tick in route_per_gender.get_xticklabels():
         tick.set_rotation(90)
     plt.savefig('images/routes_per_gender.png')
     plt.clf()
@@ -38,16 +38,16 @@ def routes_scatter(col,filename):
     ax2.set_yticklabels([0,'5.4','5.5','5.6','5.7','5.8','5.9','5.10a','5.11a','5.12a','5.13a','5.14a','5.15a'])
     ax2.set_xlabel(col)
     ax2.set_ylabel('Route grade')
-    ax2.set_title('Routes per {}'.format(col))
+    ax2.set_title('Route Grade vs. {}'.format(col))
     plt.savefig(filename)
     plt.clf()
 
 def plot_boulders():
-    bpg = sns.countplot(x='usa_boulders', hue = 'sex', data=boulders)
-    handles, _ = bpg.get_legend_handles_labels()
-    bpg.legend(handles, ["Male", "Female"])
-    bpg.set_xticklabels(['V0','V1','V2','V3','V4','V5','V6','V7','V8','V9','V10','V11','V12','V13','V14','V15','V16'])
-    bpg.set_title('Boulders per Gender')
+    boulder_per_gender = sns.countplot(x='usa_boulders', hue = 'sex', data=boulders)
+    handles, _ = boulder_per_gender.get_legend_handles_labels()
+    boulder_per_gender.legend(handles, ["Male", "Female"])
+    boulder_per_gender.set_xticklabels(['V0','V1','V2','V3','V4','V5','V6','V7','V8','V9','V10','V11','V12','V13','V14','V15','V16'])
+    boulder_per_gender.set_title('Boulder Grades per Gender')
     plt.savefig('images/boulders_per_gender.png')
     plt.clf()
 
@@ -60,10 +60,9 @@ def boulders_scatter(col,filename):
     ax.set_yticklabels(('V0','V1','V2','V3','V4','V5','V6','V7','V8','V9','V10','V11','V12','V13','V14','V15','V16'))
     ax.set_xlabel(col)
     ax.set_ylabel('Boulder grade')
-    ax.set_title('Boulders per {}'.format(col))
+    ax.set_title('Boulder Grade vs. {}'.format(col))
     plt.savefig(filename)
     plt.clf()
-
 
 def get_mean_ages(df,col):
     grades = df.groupby(col)
@@ -72,3 +71,15 @@ def get_mean_ages(df,col):
 
 boulder_mean_ages = get_mean_ages(boulders,'usa_boulders')
 route_mean_ages = get_mean_ages(routes,'usa_routes')
+
+def main():
+    plot_routes()
+    plot_boulders()
+    boulders_scatter('ascent_age','images/boulders_per_age.png')
+    boulders_scatter('height','images/boulders_per_height.png')
+    boulders_scatter('weight','images/boulders_per_weight.png')
+    boulders_scatter('time_to_send','images/boulders_send_time.png')
+    routes_scatter('ascent_age','images/routes_per_age.png')
+    routes_scatter('height','images/routes_per_height.png')
+    routes_scatter('weight','images/routes_per_weight.png')
+    routes_scatter('time_to_send','images/routes_send_time.png')
