9ae212f3dbc7341abf035affd0cab6b593ce02a4
Kelly Lutz <Kelly@MacBook-Air.local>
2018-10-09 15:19:21 -0600

    update model code

--- src/grade_model_updated.py
+++ src/grade_model_updated.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-from sklearn.linear_model import Ridge, Lasso, RidgeCV, LassoCV
+from sklearn.linear_model import RidgeClassifier, Lasso, RidgeClassifierCV, LassoCV
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.base import clone
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, r2_score
 from sklearn.metrics import mean_squared_error as mse
-import utils
+from sklearn.preprocessing import StandardScaler
 
 import matplotlib as mpl
 mpl.rcParams.update({
@@ -40,57 +40,37 @@ def split_data_grades(df,target_column):
 
 def run_ridge_model(X,y):
     X_train, X_test, y_train, y_test = train_test_split(X, y)
-    standardizer = utils.XyScaler()
-    standardizer.fit(X_train,y_train)
-    X_train_std, y_train_std = standardizer.transform(X_train, y_train)
-    X_test_std, y_test_std = standardizer.transform(X_test, y_test)
+    standardizer = StandardScaler()
+    standardizer.fit(X_train)
+    X_train_std = standardizer.transform(X_train)
+    X_test_std = standardizer.transform(X_test)
 
-    ridge = RidgeCV(alphas = np.logspace(-2,4,num=250),cv=10)
-    ridge.fit(X_train_std,y_train_std)
-    y_hats_std = ridge.predict(X_test_std)
-    X_test, y_hats = standardizer.inverse_transform(X_test_std,y_hats_std)
-    ridge_score = r2_score(y_test_std,y_hats_std)
+    ridge = RidgeClassifierCV(alphas = np.logspace(-2,4,num=250),cv=10)
+    ridge.fit(X_train_std,y_train)
+    y_hats = ridge.predict(X_test_std)
+    ridge_score = r2_score(y_test,y_hats)
     return ridge, ridge_score, y_hats, y_test, X_test
-
-def run_lasso_model(X,y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-    standardizer = utils.XyScaler()
-    standardizer.fit(X_train,y_train)
-    X_train_std, y_train_std = standardizer.transform(X_train, y_train)
-    X_test_std, y_test_std = standardizer.transform(X_test, y_test)
-
-    lasso = LassoCV(alphas = np.logspace(-2,4,num=250),cv=10)
-    lasso.fit(X_train_std,y_train_std)
-    y_hats_std = lasso.predict(X_test_std)
-    X_test, y_hats = standardizer.inverse_transform(X_test_std,y_hats_std)
-    lasso_score = r2_score(y_test_std,y_hats_std)
-    return lasso, lasso_score, y_hats, y_test, X_test
 
 def get_boulder_models(X_train,y_train):
     boulder_ridge, boulder_ridge_score, ridge_y_hats, ridge_y_true, ridge_X_test = run_ridge_model(X_train,y_train)
-    boulder_lasso, boulder_lasso_score,lasso_y_hats, lasso_y_true, lasso_X_test = run_lasso_model(X_train,y_train)
-    return boulder_ridge, boulder_lasso
+    return boulder_ridge
 
 def get_route_models(X_train,y_train):
     route_ridge, route_ridge_score, ridge_y_hats, ridge_y_true, ridge_X_test = run_ridge_model(X_train,y_train)
-    route_lasso, route_lasso_score,lasso_y_hats, lasso_y_true, lasso_X_test = run_lasso_model(X_train,y_train)
-    return route_ridge, route_lasso
+    return route_ridge
 
 def test_model_on_hold(X_train,y_train,X_hold,y_hold,model,alpha,title,filename):
-    standardizer = utils.XyScaler()
-    standardizer.fit(X_train,y_train)
-    X_train_std, y_train_std = standardizer.transform(X_train, y_train)
-    X_hold_std, y_hold_std = standardizer.transform(X_hold, y_hold)
+    standardizer = StandardScaler()
+    standardizer.fit(X_train)
+    X_train_std = standardizer.transform(X_train)
+    X_hold_std = standardizer.transform(X_hold)
     final_model = model(alpha)
-    final_model.fit(X_train_std,y_train_std)
-    y_pred_std = final_model.predict(X_hold_std)
-    X_hold, y_pred = standardizer.inverse_transform(X_hold_std,y_pred_std)
+    final_model.fit(X_train_std,y_train)
+    y_pred = final_model.predict(X_hold_std)
     plot_model_predictions(y_hold, y_pred, title, filename)
-    final_score = final_model.score(X_hold_std,y_hold_std)
-    final_mse_std = mse(y_hold_std,y_pred_std)
+    final_score = final_model.score(X_hold_std,y_hold)
     final_mse = mse(y_hold,y_pred)
     print('Final R2 score: {}'.format(final_score))
-    print('Final standardized RMSE: {}'.format(np.sqrt(final_mse_std)))
     print('Final RMSE: {}'.format(np.sqrt(final_mse)))
     return final_score, final_mse
 
@@ -147,21 +127,19 @@ def plot_model_predictions(y_true,y_pred,title,filename):
     plt.savefig(filename)
     plt.clf()
 
-def main_boulder(df,title1,filename1,title2,filename2):
+def main_boulder(df,title1,filename1):
     boulder = drop_columns_grades(df)
     X_train, y_train, X_hold, y_hold = split_data_grades(boulder,'usa_boulders')
-    boulder_ridge, boulder_lasso = get_boulder_models(X_train, y_train)
-    test_model_on_hold(X_train,y_train,X_hold,y_hold,Ridge,boulder_ridge.alpha_,title1,filename1)
-    test_model_on_hold(X_train,y_train,X_hold,y_hold,Lasso,boulder_lasso.alpha_,title2,filename2)
-    boulder_coefs = get_coefs(boulder_lasso,boulder.drop('usa_boulders',axis=1))
+    boulder_ridge = get_boulder_models(X_train, y_train)
+    test_model_on_hold(X_train,y_train,X_hold,y_hold,RidgeClassifier,boulder_ridge.alpha_,title1,filename1)
+    boulder_coefs = get_coefs(boulder_ridge,boulder.drop('usa_boulders',axis=1))
     print(boulder_coefs.sort_values(0))
 
-def main_route(df,title1,filename1,title2,filename2):
+def main_route(df,title1,filename1):
     route = drop_columns_grades(df)
     X_train, y_train, X_hold, y_hold = split_data_grades(route,'usa_routes')
-    route_ridge, route_lasso = get_route_models(X_train,y_train)
+    route_ridge = get_route_models(X_train,y_train)
     test_model_on_hold(X_train,y_train,X_hold,y_hold,Ridge,route_ridge.alpha_,title1,filename1)
-    test_model_on_hold(X_train,y_train,X_hold,y_hold,Lasso,route_lasso.alpha_,title2,filename2)
     route_coefs = get_coefs(route_lasso,route.drop('usa_routes',axis=1))
     print(route_coefs.sort_values(0))
 
@@ -169,13 +147,13 @@ def restrict_boulder():
     boulders, routes = import_data()
     boulders_restrict = boulders.copy()
     boulders_restrict = boulders_restrict.loc[boulders_restrict['usa_boulders'] >= 5]
-    main_boulder(boulders_restrict,'Final Ridge Prediction for Boulder grades','images/ridge_model_boulder_restrict.png','Final Lasso Prediction for Boulder grades','images/lasso_model_boulder_restrict.png')
+    main_boulder(boulders_restrict,'Final Ridge Prediction for Boulder grades','images/ridge_model_boulder_restrict.png')
 
 def restrict_route():
     boulders, routes = import_data()
     routes_restrict = routes.copy()
     routes_restrict = routes_restrict.loc[routes_restrict['usa_routes'] >= 5.11]
-    main_route(routes_restrict,'Final Ridge Prediction for Route grades','images/ridge_model_route_restrict.png','Final Lasso Prediction for Route grades','images/lasso_model_route_restrict.png')
+    main_route(routes_restrict,'Final Ridge Prediction for Route grades','images/ridge_model_route_restrict.png')
 
 def add_features(df):
     descriptions = ['soft','hard']
@@ -190,22 +168,22 @@ def add_features(df):
 def add_features_model_boulder():
     boulders, routes = import_data()
     boulder = add_features(boulders)
-    main_boulder(boulder,'Ridge Prediction for Boulder grades','images/ridge_model_boulder_add.png','Lasso Prediction for Boulder grades','images/lasso_model_boulder_add.png')
+    main_boulder(boulder,'Ridge Prediction for Boulder grades','images/ridge_model_boulder_add.png')
 
 def add_features_model_route():
     boulders, routes = import_data()
     route = add_features(routes)
-    main_route(route,'Ridge Prediction for Route grades','images/ridge_model_route_add.png','Lasso Prediction for Route grades','images/lasso_model_route_add.png')
+    main_route(route,'Ridge Prediction for Route grades','images/ridge_model_route_add.png')
 
 def run_all_boulder_models():
     boulders, routes = import_data()
-    main_boulder(boulders,'Ridge Prediction for Boulder grades','images/ridge_model_boulder_final.png','Lasso Prediction for Boulder grades','images/lasso_model_boulder_final.png')
+    main_boulder(boulders,'Ridge Prediction for Boulder grades','images/ridge_model_boulder_final.png')
     restrict_boulder()
     add_features_model_boulder()
 
 def run_all_route_models():
     boulders, routes = import_data()
-    main_route(routes,'Ridge Prediction for Route grades','images/ridge_model_route_final.png','Lasso Prediction for Route grades','images/lasso_model_route_final.png')
+    main_route(routes,'Ridge Prediction for Route grades','images/ridge_model_route_final.png')
     restrict_route()
     add_features_model_route()
 
@@ -226,4 +204,4 @@ def edit_route_grades():
     for col in dates:
         routes[col] = routes[col].astype('datetime64')
 
-    main_route(routes,'Ridge Prediction for Route grades','images/ridge_model_route_updated.png','Lasso Prediction for Route grades','images/lasso_model_route_updated.png')
+    main_route(routes,'Ridge Prediction for Route grades','images/ridge_model_route_updated.png')
